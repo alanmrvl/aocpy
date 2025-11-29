7a2568ac00db2be7ba3fae45ff0dc6d1115aa979
Alan Marvel <alan@mrvl.io>
2025-11-28 21:45:57 -0500

    Question 1a

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from aocpy!")
+import sys
 
+filename = sys.argv[1]
 
-if __name__ == "__main__":
-    main()
+left = []
+right = []
+
+with open(filename) as file:
+    for line in file:
+        [l, r] = line.split()
+        left.append(int(l))
+        right.append(int(r))
+
+left.sort()
+right.sort()
+
+sum = 0
+
+for i in range(0, len(left)):
+    sum += abs(left[i] - right[i])
+
+print(sum)
